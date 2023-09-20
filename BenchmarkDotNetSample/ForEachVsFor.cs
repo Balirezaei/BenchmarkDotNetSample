592401fa8e630fefa91fa1a6aa10cb04ae3e8086
Bahar Alirezaei <b.alirezaei@sadad.co.ir>
2023-09-20 14:33:17 +0330

    init

---
+++ BenchmarkDotNetSample/ForEachVsFor.cs
@@ -0,0 +1,44 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Reports;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchmarkDotNetSample
+{
+
+    [MemoryDiagnoser]
+    [Orderer(BenchmarkDotNet.Order.SummaryOrderPolicy.FastestToSlowest)]
+    [RankColumn]
+    public class ForEachVsFor
+    {
+      
+        [Benchmark(Baseline = true)]
+        public void ForEachAsync()
+        {
+            var i = 0;
+            foreach (var item in Enumerable.Range(0, 1000000))
+            {
+                i++;
+            }
+
+        }
+
+
+        [Benchmark]
+        public void ForAsync()
+        {
+            var i = 0;
+            for (var j = 0; j <= 1000000; j++)
+            {
+                i++;
+            }
+
+        }
+
+    }
+}
---
+++ BenchmarkDotNetSample/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+using BenchmarkDotNetSample;
+
+var config =  new ManualConfig()
+     .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+     .AddValidator(JitOptimizationsValidator.DontFailOnError)
+     .AddLogger(ConsoleLogger.Default)
+     .AddColumnProvider(DefaultColumnProviders.Instance);
+
+Console.WriteLine(BenchmarkRunner.Run<ForEachVsFor>(config));
+
+
+
+Console.ReadLine();
